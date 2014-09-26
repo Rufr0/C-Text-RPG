65b1eb89707f36aed53e9ff1e873bdf4a1cf4708
Rufr <ANTHONY@anthonys-MacBook-Air.local>
2014-09-25 20:45:08 -0400

    1-n

--- CsharpRPG/Program.cs
+++ CsharpRPG/Program.cs
@@ -7,54 +7,54 @@ namespace CsharpRPG
 		public static void Main (string[] args)
 
 		{
-			string responce;
-			string location;
-			string ocupation = "idk";
-			Double money = 0.00;
+			string location = "Project select";
+			string [] projects= new string[1];
 
-			System.Console.Write("Traveler, what do you call yourself?\t");
-			string name = System.Console.ReadLine ();
-			System.Console.Write ("[Guard]\t"+name + ", Welcome to the magical land of kelwitch, I see you have some hay are you here to traid?\t");
-			if (System.Console.ReadLine () == "yes")
-			{
-				string ocupation = "traider";
-				Double money = 150.00;
-				System.Console.WriteLine ("[Guard]\tWell then, since I know so much about you, you deserver to know something about me!\n\tI'll tell you on the way to the market, how about that?");
-				location = "market";
+			projects [0] = "1-n";
+
+
+
+			while (true) {
+				if (location == "Project select") {
+					System.Console.WriteLine ("PROJECTS:");
+					foreach (string value in projects) {
+						System.Console.WriteLine (value);
+					}
+					string responce=System.Console.ReadLine ();
+
+					bool found = false;
+					foreach (string value in projects) {
+						if (value == responce)
+							found = true;
+					}
+					if (found == false) {
+						System.Console.WriteLine ("\nNot on list\n");
+					}
+					else
+						location=responce;
+				}
+
+
+				if (location == "1-n") {
+					System.Console.Write ("Enter a number to count from: ");
+					int lowernumber = Convert.ToInt32 (System.Console.ReadLine ());
+					System.Console.Write ("Enter a number to count to: ");
+					int highernumber = Convert.ToInt32 (System.Console.ReadLine ());
+					for (; lowernumber <= highernumber; lowernumber++) {
+						System.Console.Write ("{0,6}",lowernumber);
+					}
+					System.Console.ReadLine ();
+					location = "Project select";
+				}
+
+
+
+
+
+
+
+
 			}
-			else
-			{
-				System.Console.WriteLine("[Guard]\tWell then, what ya doing with that hay?\n[a]\tI bought it...\n[b]\tI have hay... wow I never knew!\n[c]\tMy buisness and none of yours!");
-				responce =System.Console.ReadLine().ToLower();
-				if (responce=="a")
-					System.Console.WriteLine("[Guard]\tHmm... very well then...");location="entrance"
-				else if (responce=="b")
-					System.Console.WriteLine("[Guard]\t"+"Don\'t play dumb with me now! You\'d be luck I didn\'t send you to jail for that!");location="entrance"
-				else if (responce=="c")
-					System.Console.WriteLine("[Guard]\tWell its our buisness as guards to inspect you, come with me, I am making it our buisness!");location="jail"
-				
-			}
-
-			while (true)
-			{
-				if (location=="jail")
-					System.Console.WriteLine("You have been thrown in jail, with no hope of freedom. Failure.");
-
-				if (location=="market");
-			}
-				
-			
-
-
-
-
-
-
-
-
-
-
-
 		}
 	}
 }
